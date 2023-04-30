ff92197b13ed0b0e58089b2e4b1388cf594c1bc1
Grian Gajila <griangajila2@gmail.com>
2023-04-30 19:50:49 +0800

    Final: Adding relay

--- ECOBOT.ino
+++ ECOBOT.ino
@@ -1,12 +1,7 @@
-ahh
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h> 
-#include <Firebase.h>
-#define FIREBASE_HOST "ecobotproject-9586f-default-rtdb.firebaseio.com"         
-#define FIREBASE_AUTH "DEOZcC6qIokR8osdddXq0TwL868SzVhv15PmOSjo"   
+#include <ESP8266WebServer.h>
 #include <Servo.h>
-
 
 const char* ssid = "realme C12";
 const char* password = "00000000";
@@ -16,40 +11,37 @@ ESP8266WebServer server(80);
 Servo servo1; 
 Servo servo2;
 
-#define relay1  D2
-#define relay2  D0
-#define relay3  D1
-
-int value1;
-int value2;
-int value3;
-int value4;
+#define relay1  D0
+#define relay2  D1
+#define relay3  D2
+#define relay4  D3
 
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   Serial.println("");
-  
+
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
   }
    pinMode(LED_BUILTIN, OUTPUT);
-   
+
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  servo1.attach(D3);
-  servo2.attach(D4);
+  servo1.attach(D4);
+  servo2.attach(D5);
 
   pinMode(relay1, OUTPUT); 
   pinMode(relay2, OUTPUT);
   pinMode(relay3, OUTPUT);
-  
+  pinMode(relay4, OUTPUT);
+
   server.on("/", HTTP_handleRoot);
   server.onNotFound(HTTP_handleRoot);
   server.begin();
@@ -71,9 +63,9 @@ void loop() {
     int servo2Value = map(command.toInt(), 0, 100, 0, 180); 
     servo2.write(servo2Value);
   }
-  
+
   server.handleClient();
-  
+
 }
 
 void HTTP_handleRoot(void) {
@@ -84,37 +76,38 @@ void HTTP_handleRoot(void) {
   delay(1);
 }
 
-void ON() { 
-  digitalWrite(relay1, HIGH);
-   digitalWrite(LED_BUILTIN, LOW); 
-   delay(5);
-}
-
-void OFF() { 
-  digitalWrite(relay1, LOW);
-   digitalWrite(LED_BUILTIN, HIGH); 
-   delay(5);
- 
-}
 
 void UPON() { 
-  digitalWrite(relay2, LOW); 
+   digitalWrite(relay1, LOW); 
+   digitalWrite(relay3, LOW); 
    digitalWrite(LED_BUILTIN, HIGH); 
    delay(5);
 }
 
 void DOWNON() {
-  digitalWrite(relay3, LOW);
+   digitalWrite(relay2, LOW);
+   digitalWrite(LED_BUILTIN, HIGH); 
+   delay(5);
+
+}
+
+void ON() { 
+   digitalWrite(relay4, HIGH);
+   digitalWrite(LED_BUILTIN, LOW); 
+   delay(5);
+}
+
+void OFF() { 
+   digitalWrite(relay4, LOW);
    digitalWrite(LED_BUILTIN, HIGH); 
    delay(5);
 
 }
 
 void off() {
-    digitalWrite(relay1, HIGH);
+  digitalWrite(relay1, HIGH);
   digitalWrite(relay2, HIGH);
   digitalWrite(relay3, HIGH);
    digitalWrite(LED_BUILTIN, LOW); 
    delay(5);
-
 }
